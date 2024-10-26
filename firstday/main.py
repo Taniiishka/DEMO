a2610760413120a228da173d8401d2fb0c928611
gtanishka160@gmial.com <YourEmail@example.com>
2024-10-26 19:09:31 +0530

    first day

--- firstday/main.py
+++ firstday/main.py
@@ -11,10 +11,26 @@
 
 # multiplication
 #a = int(input("enter your choice:"))
-#b = int(input("enter your choice:"))
+#b = int(input("enter your choice:")) #int is used for typecasting
 #print("multiply",a*b)
 
 # divide
-a = int(input("enter your choice:"))
-b = int(input("enter your choice:"))
-print("divide",a/b)
+# a = int(input("enter your choice:"))
+# b = int(input("enter your choice:"))
+# print("divide",a/b)
+
+#import keyword
+#keyword.kwlist
+
+string = "Hello, World!"
+substring = string[7:12]
+print(substring)
+
+
+
+
+
+
+
+
+
